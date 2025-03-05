94185769f8c8a83513dc5dbc848a75c459214c20
andrazmehle <andrazmehle05@gmail.com>
2025-03-05 21:04:37 +0100

    Initial commit

---
+++ src/KVIZI/Kviz1.java
@@ -0,0 +1,125 @@
+package KVIZI;
+
+public class Kviz1{
+
+    public static void krog(double r, int d){
+        if(r < 0) {
+            System.out.println("Napaka: negativen polmer");
+            return;
+        }
+        if(d < 0 ){
+            System.out.println("Napaka: negativen d");
+            return;
+        }
+
+        double obsegKroga = 2 * Math.PI * r;
+        double ploscinaKroga = Math.PI * Math.pow(r, 2);
+
+        String format = "Obseg kroga s polmerom r=%.2f je %." + d + "f \n";
+        System.out.printf(format, r, obsegKroga);
+        System.out.printf(format, r, obsegKroga);
+
+        String format2 = "Ploscina kroga s polmerom r=%.2f je %." + d + "f";
+        System.out.printf(format2, r, ploscinaKroga);
+    }
+
+    public static String pretvoriSekunde(int sekunde){
+        if (sekunde < 0) {
+            return "Število sekund ne more biti negativno";
+        }
+
+        int ure = sekunde / 3600;
+        int minute = sekunde / 60 - ure * 60;
+
+        return String.format("%02d:%02d:%02d", ure, minute, sekunde % 60);
+    }
+
+    public static void javaJavaJava(int n) {
+        if (n < 0) {
+            System.out.println("Napaka: negativen n");
+            return;
+        }
+
+        String prva = "     J    a   v     v  a   ";
+        String druga = "     J   a a   v   v  a a  ";
+        String tretja = "  J  J  aaaaa   V V  aaaaa ";
+        String cetrta = "   JJ  a     a   V  a     a";
+
+        String prva1 = "";
+        String druga1 = "";
+        String tretja1 = "";
+        String cetrta1 = "";
+
+        while (n > 0) {
+            prva1 += prva;
+            druga1 += druga;
+            tretja1 += tretja;
+            cetrta1 += cetrta;
+            n--;
+        }
+
+        System.out.println(prva1);
+        System.out.println(druga1);
+        System.out.println(tretja1);
+        System.out.println(cetrta1);
+
+    }
+
+    public static boolean jeFibonaccijevo(int n){
+        int j = 1;
+        int i = 1;
+        int k;
+
+        while (i <= n){
+            if (n == i){
+                return true;
+            }
+            k = j;
+            j = i;
+            i = j + k;
+
+        }
+        return false;
+    }
+
+    public static boolean jePrastevilo(int n){
+        if(n < 0){
+            return false;
+        }
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void izrisiZastavo(int n){
+        for (int i = 0; i < 5 * n; i++) {
+            if(i < 3 * n){
+                if(i % 2 == 0){
+                    System.out.print("* ".repeat(n * 2));
+                    System.out.print("=".repeat(13 * n - 1) + "\n");
+                }
+                else{
+                    System.out.print(" *".repeat(n) + "  ");
+                    System.out.print("=".repeat(13 * n - 1) + "\n");
+                }
+            }
+            else{
+                System.out.println("=".repeat(15 * n + 1));
+            }
+
+        }
+    }
+
+        public static void main(String[]args){
+            //krog(3, 5);
+            //System.out.println(pretvoriSekunde(49330));
+            //javaJavaJava(5);
+            //System.out.println(jeFibonaccijevo(2021));
+            //System.out.println(jePrastevilo(15));
+            izrisiZastavo(2);
+        }
+    }
