cfd629186fe60108271fe05a941bf4bd6b95ef6d
andrazmehle <andrazmehle05@gmail.com>
2025-03-17 08:36:12 +0100

    vaje2

---
+++ src/VAJE/vaje2.java
@@ -0,0 +1,77 @@
+package VAJE;
+
+public class vaje2 {
+
+    static long fakultetaL(int n){
+        long fakulteta = 1;
+        for (int i = 1; i <= n; i++) {
+            fakulteta *= i;
+        }
+        return fakulteta;
+    }
+
+    static long stirlingL(int n){
+        double fakulteta = Math.sqrt(2 * Math.PI * n) * Math.pow((n / Math.E), n);
+        return Math.round(fakulteta);
+    }
+
+    static double fakultetaD(int n){
+        double fakulteta = 1;
+        for (int i = 1; i <= n; i++) {
+            fakulteta *= i;
+        }
+        return fakulteta;
+    }
+
+    static double stirlingD(int n){
+        double fakulteta = Math.sqrt(2 * Math.PI * n);
+        return Math.round(fakulteta);
+    }
+
+    static void izpis(){
+        double napaka;
+
+        System.out.println("  n              n!            Stirling(n)      napaka (%)");
+        System.out.println("----------------------------------------------------------");
+
+        for (int i = 1; i <= 20; i++) {
+            long fL = fakultetaL(i);
+            long sL = stirlingL(i);
+            napaka = (fL - sL + 0.0) / fL;
+
+            System.out.printf("%3d", i);
+            System.out.printf("%21d", fL);
+            System.out.printf("%21d", sL);
+            System.out.printf("%12.7f", napaka * 100);
+            System.out.println();
+        }
+    }
+
+    static void izpisD(){
+        double napaka;
+
+        System.out.println("  n              n!            Stirling(n)      napaka (%)");
+        System.out.println("----------------------------------------------------------");
+
+        for (int i = 1; i <= 100; i++) {
+            double fL = fakultetaD(i);
+            double sL = stirlingD(i);
+            napaka = (fL - sL + 0.0) / fL;
+
+            System.out.printf("%3d", i);
+            System.out.printf("%21.9E", fL);
+            System.out.printf("%21.9E", sL);
+            System.out.printf("%12.7f", napaka * 100);
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        //System.out.println(fakultetaL(10));
+        //System.out.println(stirlingL(10));
+
+        //izpis();
+        izpisD();
+    }
+}
